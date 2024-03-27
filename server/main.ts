6aa89d7194555a31a37a7cbef8f76ab32dc52155
Timm TheBlokker <theblokker.dev@gmail.com>
2024-03-27 10:19:16 +0100

    remove old player object

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,7 @@
 /// <reference types="@altv/types-server" />
 
 import * as alt from 'alt-server';
-import { PlayerData } from '../../tr-core/server/player';
+import {  } from '../../tr-core/server/player';
 
 let Drops = {}
 let Trunks = {}
